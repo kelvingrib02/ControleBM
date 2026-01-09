95108495c91f8e73125a4e3b851c89b190dcdcdd
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2026-01-08 23:32:58 -0300

    Adicionando controller de produtos

---
+++ src/ControleBM.API/Controllers/ProdutosController.cs
@@ -0,0 +1,37 @@
+﻿using ControleBM.Domain.Entities;
+using ControleBM.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleBM.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Produto>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            if (produto.PrecoUnitario <= 0)
+                return BadRequest("O preço de venda deve ser maior que zero.");
+
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProdutos), new { id = produto.Id }, produto);
+        }
+    }
+}
--- src/ControleBM.Application/obj/Debug/net10.0/ControleBM.Application.AssemblyInfo.cs
+++ src/ControleBM.Application/obj/Debug/net10.0/ControleBM.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleBM.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c60169611d4348ce6aaa48b1c5b4884c0e2760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef3ec30e577167351c45270cdec37553993ec25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleBM.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleBM.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
