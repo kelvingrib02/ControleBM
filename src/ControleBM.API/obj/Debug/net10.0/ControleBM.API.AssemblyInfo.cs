800d5e63e163fbf59a9925378ef639ab070f4ab3
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2026-03-24 01:35:17 -0300

    Atualizando intens pontuais

--- src/ControleBM.API/obj/Debug/net10.0/ControleBM.API.AssemblyInfo.cs
+++ src/ControleBM.API/obj/Debug/net10.0/ControleBM.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleBM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2254cbcbc512860b139a910e1d8e2d6c4ce7d4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be80e9ff694f47d30d0c1826d884209520274554")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleBM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleBM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
